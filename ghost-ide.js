f8cf60c76b1ef1f6e5faaac6d20b5820af479292
Ghost <ismaelkelvyn328@gmail.com>
2026-04-21 23:01:48 -0300

    Update ghost-ide.js

--- ghost-ide.js
+++ ghost-ide.js
@@ -1,48 +1,25 @@
-const commands = {};
-
-async function loadCommands() {
-    const res = await fetch('./cmd/manifest.json');
-    const data = await res.json();
-
-    for (let cmd of data.commands) {
-        const module = await import(`./cmd/${cmd}.js`);
-        commands[cmd] = module.run;
-    }
-}
-
 // Troca de abas
 function switchTab(lang) {
-    document.querySelectorAll('.tab-btn').forEach(b => b.classList.remove('active'));
-    document.querySelectorAll('.editor-pane').forEach(p => p.classList.remove('active'));
-    
-    document.querySelector(`[onclick="switchTab('${lang}')"]`).classList.add('active');
-    document.getElementById(`pane-${lang}`).classList.add('active');
+document.querySelectorAll('.tab-btn').forEach(b => b.classList.remove('active'));
+document.querySelectorAll('.editor-pane').forEach(p => p.classList.remove('active'));
+
+document.querySelector(`[onclick="switchTab('${lang}')"]`).classList.add('active');  
+document.getElementById(`pane-${lang}`).classList.add('active');
+
 }
 
 // Preview
-async function runCode() {
-    console.log("clicou");
+function runCode() {
+const html = document.getElementById('htmlCode').value;
+const css = document.getElementById('cssCode').value;
+const js = document.getElementById('jsCode').value;
+const frame = document.getElementById('preview');
 
-    let html = document.getElementById('htmlCode').value;
-    const css = document.getElementById('cssCode').value;
-    const js = document.getElementById('jsCode').value;
+frame.srcdoc = `<html><head><style>${css}</style></head><body>${html}<script>${js}<\/script></body></html>`;  
+document.getElementById('viewport').style.display = 'block';
 
-    const frame = document.getElementById('preview');
-
-    frame.srcdoc = `
-        <html>
-        <head>
-            <style>${css}</style>
-        </head>
-        <body>
-            ${html}
-            <script>${js}<\/script>
-        </body>
-        </html>
-    `;
-
-    document.getElementById('viewport').style.display = 'block';
 }
+
 function closePreview() {
-    document.getElementById('viewport').style.display = 'none';
+document.getElementById('viewport').style.display = 'none';
 }
